8fa9737d0271055c77f73ca9052428ccb4fc7183
Lungisa Nduna <39986192+LungisaNduna@users.noreply.github.com>
2019-09-29 23:47:48 +0200

    Updates

---
+++ precache-manifest.120daaa8bc50993acafaa4ee0f71ecc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30975a17caffe9742a042b82d380cf6f",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "1e16a8bcdd3420ecfdfb",
+    "url": "/profile/static/css/main.6163143c.chunk.css"
+  },
+  {
+    "revision": "2248f701c57b6852abdd",
+    "url": "/profile/static/js/2.a9e899bd.chunk.js"
+  },
+  {
+    "revision": "1e16a8bcdd3420ecfdfb",
+    "url": "/profile/static/js/main.19d0c41d.chunk.js"
+  },
+  {
+    "revision": "63b28193354c61073fb2",
+    "url": "/profile/static/js/runtime-main.9da9808d.js"
+  },
+  {
+    "revision": "a5424aecfee96c6d756a233862d16d13",
+    "url": "/profile/static/media/LungisaProfile.a5424aec.jpg"
+  },
+  {
+    "revision": "9508502bb1293b80d2638a51bd58216f",
+    "url": "/profile/static/media/prof_background.9508502b.jpg"
+  }
+]);
