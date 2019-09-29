9e263a41992e74cb9d18f260e380009746b630d0
Lungisa Nduna <39986192+LungisaNduna@users.noreply.github.com>
2019-09-30 00:56:16 +0200

    add about me section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { Header } from './Components/TemplateComponents/Header';
+import AboutMe from './Components/TemplateComponents/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe/>
     </div>
   );
 }
