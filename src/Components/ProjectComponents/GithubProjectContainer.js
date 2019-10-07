e05009e62eb94d4fac96541bd8189e400a54d875
Lungisa Nduna <Lungisa.Nduna@britehouse.co.za>
2019-10-07 11:56:40 +0200

    moved call out for testability

--- src/Components/ProjectComponents/GithubProjectContainer.js
+++ src/Components/ProjectComponents/GithubProjectContainer.js
@@ -1,47 +1,25 @@
-import React, { Component, lazy, Suspense } from 'react'
-import ProjectRepository from '../../Repositories/githubProject.Repository';
-//import GithubProjectCard from './GithubProjectCard';
-const GithubProjectCard = lazy(()=> import("./GithubProjectCard"));
+import React, { Component } from 'react'
+import {GithubProjectCard} from "./GithubProjectCard";
+import UnsplashLink from '../smallComponents/UnsplashLink';
 
 export class GithubProjectContainer extends Component {
     constructor(props){
         super(props);
-        this.state = {
-            githubProjects : null,
-            isReady : false
-        }
-    }
-
-    componentDidMount(){
-        fetch(`https://api.github.com/users/LungisaNduna/repos`)
-        .then(response => response.json())
-        .then(parsedJSON => new ProjectRepository(parsedJSON))
-        .then(ghProjRepo => {
-            let highlightedProjects = ["profile", "digital-cards", "push_swap", "UnityFundamentals", "filler"]
-            let githubProjects = ghProjRepo.returnHighlightProjectList(highlightedProjects);
-            this.setState({ githubProjects });
-            this.setState({ isReady : true })
-        })
+        console.log("making the container");
     }
 
     render() {
+       const {githubProjects} = this.props; 
         return (
             <div className="container" id="GithubProjContainer">
+                {/* Just placing a header */}
                 <h1>Some Projects I've Done</h1>
+                {/* mapping a project card for each project we have data for */}
                 <div  id="GithubProjSection">
-                    {this.state.isReady && this.state.githubProjects.map(proj => 
-                        <Suspense fallback={<div>Loading Project</div>}><GithubProjectCard projInfo={proj}/></Suspense> 
-                    )}
+                    {githubProjects.map(proj => <GithubProjectCard projInfo={proj}/>)}
                 </div>
-                <a className="photoLink" href="https://unsplash.com/@mxhpics?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Maxime Horlaville">
-                    <span className="photolink_span1">
-                        <svg className="photoLink_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-                            <title>unsplash-logo</title>
-                            <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
-                        </svg>
-                    </span>
-                    <span className="photoLink_span2">photo by Maxime Horlaville</span>
-                </a>
+                {/* the link from Unsplash. Got the component and the */}
+                <UnsplashLink linkAddress="https://unsplash.com/@mxhpics?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" Artist="Maxime Horlaville" />
             </div>
         )
     }
