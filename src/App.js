33de5d890e85d05c1c994472e133c56055ad942d
Lungisa Nduna <Lungisa.Nduna@britehouse.co.za>
2019-10-08 17:01:16 +0200

    added switch for styling feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './App.css';
+
 import { Header } from './Components/TemplateComponents/Header';
 import AboutMe from './Components/TemplateComponents/AboutMe';
 import GithubProjectContainer from './Components/ProjectComponents/GithubProjectContainer';
 import ProjectRepository from './Repositories/githubProject.Repository';
-
-import { image } from './Resources/LungisaProfile.jpg';
 import ImageModal from './Components/smallComponents/ImageModal';
+
+import AfroIcon from './Resources/afro.svg'
+import SpeechBubble from './Resources/love.svg'
 
 export class App extends Component {
   constructor(props){
@@ -17,6 +19,7 @@ export class App extends Component {
       gitDisplayReady : false,
       ghProjRepository : null,
       showImageModal : false,
+      profLook: true,
     }
   }
 
@@ -34,13 +37,20 @@ export class App extends Component {
     this.setState({ showImageModal: !this.state.showImageModal });
   }
 
+  changeStyling = (switchState) => {
+    this.setState({ profLook: switchState });
+    let style = (this.state.profLook) ? "Professional look" : "Expressive look";
+    alert("The styling has been switched to the "+style);
+    //code to change the state
+  }
+
   render() {
-    const {ghProjRepository} = this.state;
+    const {ghProjRepository, profLook} = this.state;
     const highlightGHProjects = ["profile", "digital-cards", "push_swap", "UnityFundamentals", "filler"];
     return (
       <div className="App">
         
-        <Header />
+        <Header handleSwitch={this.changeStyling} profLook={profLook} />
         
         <AboutMe openModal={this.toggleModal} /> 
         
@@ -54,6 +64,12 @@ export class App extends Component {
           show={this.state.showImageModal}
           onClose={this.toggleModal}
         />
+
+        <div className="AppFooter">
+          <h2>Some other Credits</h2>
+          <div><img className="CreditIcon" src={AfroIcon} alt="" /> made by <a href="https://www.flaticon.com/<?=_('authors/')?>smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          <div><img className="CreditIcon" src={SpeechBubble} alt="" /> made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"         title="Flaticon">www.flaticon.com</a></div>
+        </div>
       
       </div>
     )
--- src/Components/TemplateComponents/Header.js
+++ src/Components/TemplateComponents/Header.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react'
 import LungisaProfile from '../../Resources/LungisaProfile.jpg';
 import UnsplashLink from '../smallComponents/UnsplashLink';
+import Switch from 'react-switch';
+
 export class Header extends Component {
 
+    constructor(props){
+        super(props);
+        this.state = { checked: true, }
+    }
+
+    handleChange = (checked) => {
+        this.setState({ checked });
+        this.props.handleSwitch(checked);
+    }
     render(){
         //for speech component look into https://www.npmjs.com/package/react-speech
         return(
             <header className="App-header">
                 
                 <h1>Lungisa Nduna</h1>
+                <div className="StyleSwitchDiv">
+                    <span className="StyleSwitchSpan">The look is set to {(this.state.checked) ? "Professional" : "Expressive" } </span>
+                    <Switch 
+                        onChange={this.handleChange} 
+                        checked={this.state.checked}
+                        uncheckedIcon={
+                            <div
+                              style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100%",
+                                fontSize: 15,
+                                color: "orange",
+                                paddingRight: 2
+                              }}
+                            >
+                              Exp
+                            </div>
+                          }
+                          checkedIcon={
+                            <div
+                              style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                height: "100%",
+                                fontSize: 15,
+                                color: "blue",
+                                paddingRight: 2
+                              }}
+                            >
+                              Prof
+                            </div>
+                          }
+                    />
+                </div> 
                 <img className="ProfilePic" src={LungisaProfile} alt="Lungisa's face" />
                 <p>This is my profile page standing in as my online resume.</p>
 
